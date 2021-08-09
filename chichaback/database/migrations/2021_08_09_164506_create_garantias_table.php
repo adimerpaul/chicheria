2663bce00dd7dadf3524f869a8002ebc278d89eb
adimerpaul <adimer101@gmail.com>
2021-08-09 16:48:08 -0400

    modificado para garantia

---
+++ chichaback/database/migrations/2021_08_09_164506_create_garantias_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGarantiasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('garantias', function (Blueprint $table) {
+            $table->id();
+            $table->date("fecha")->nullable();
+            $table->double("efectivo",11,2)->nullable();
+            $table->string("fisico")->nullable();
+            $table->string("detalle")->nullable();
+            $table->string("estado")->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('garantias');
+    }
+}
