2488aa0a8c4424bd4d6d1deacf3c31d7f805e9f5
adimerpaul <adimer101@email.com>
2021-08-12 22:03:42 -0400

    Update quasar.conf.js

--- chichafront/quasar.conf.js
+++ chichafront/quasar.conf.js
@@ -73,7 +73,7 @@ module.exports = configure(function (ctx) {
       env: {
         API: ctx.dev
           ? 'http://localhost:8000/api'
-          : 'https://prod.api.com'
+          : 'http://165.22.188.126:82/api'
       }
     },
 
