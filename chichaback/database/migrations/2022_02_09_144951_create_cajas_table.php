e7847f5eae340ffb11a55d64f5f3252684ab6ab8
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-02-11 21:24:11 -0400

    NEW

---
+++ chichaback/database/migrations/2022_02_09_144951_create_cajas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCajasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cajas', function (Blueprint $table) {
+            $table->id();
+            $table->double('total')->default(0);
+            $table->double('monto')->default(0);
+            $table->string('observacion')->default('');
+            $table->date('fecha');
+            $table->time('hora');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cajas');
+    }
+}
