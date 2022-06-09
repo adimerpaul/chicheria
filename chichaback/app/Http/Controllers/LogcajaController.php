837759dd8ae234a83bc2dba569e50f72988fa5e2
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-06-09 16:42:00 -0400

    PERMISO

--- chichaback/app/Http/Controllers/LogcajaController.php
+++ chichaback/app/Http/Controllers/LogcajaController.php
@@ -29,7 +29,7 @@ class LogcajaController extends Controller
         if($request->user()->id==1)
         {
             if($request->id==0) return Logcaja::with('user')->whereDate('fecha','>=',$request->fecha1)->whereDate('fecha','<=',$request->fecha2)->get();
-            else return Logcaja::with('user')->where('user_id',$request->user()->id)->whereDate('fecha','>=',$request->fecha1)->whereDate('fecha','<=',$request->fecha2)->get();
+            else return Logcaja::with('user')->where('user_id',$request->id)->whereDate('fecha','>=',$request->fecha1)->whereDate('fecha','<=',$request->fecha2)->get();
         }
         else
         return Logcaja::with('user')->where('user_id',$request->user()->id)->whereDate('fecha','>=',$request->fecha1)->whereDate('fecha','<=',$request->fecha2)->get();
