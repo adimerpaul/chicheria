e3cc13f63b2bc9ea47c034f878094604fb3c8ad8
@lopez.gutierrez.a07 <lopez.gutierrez.a07@gmail.com>
2023-10-27 21:11:41 -0400

    subir

--- chichaback/app/Http/Controllers/LogcompraController.php
+++ chichaback/app/Http/Controllers/LogcompraController.php
@@ -57,6 +57,8 @@ class LogcompraController extends Controller
         $logcompra->save();
 
         $compra=Compra::find($request->compra_id);
+        if($compra->deuda> $request->monto)
+            return false;
         $compra->deuda=$compra->deuda + $request->monto;
 
         if($request->checktipo=='GASTO'){
@@ -118,7 +120,7 @@ class LogcompraController extends Controller
         <table>
         <tr><td>Fecha Pago: </td><td><b>'.date('d/m/Y',strtotime($logcompra->fecha)).'</b></td></tr>
         <tr><td>Monto: </td><td><b>'.$logcompra->monto.'</b></td></tr>
-        <tr><td>Saldo: </td><td><b>'.$compra->subtotal - $compra->deuda.'</b></td></tr>
+        <tr><td>Saldo: </td><td><b>'(floatval($compra->subtotal) - floatval($compra->deuda)).'</b></td></tr>
         <tr><td>Observacion: </td><td><b>'.$logcompra->observacion.'</b></td></tr>
         </table>
         <br>
