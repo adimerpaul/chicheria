3099e61fdab7d645b9511f32abfd76a31f237b9c
adimerpaul <adimer101@gmail.com>
2025-05-07 06:48:29 -0400

    cocaod sql de permiso

--- permiso.sql
+++ permiso.sql
@@ -21,3 +21,13 @@ UPDATE permisos SET grupo = 'Préstamos' WHERE id IN (12, 16, 27);
 
 -- 🧱 Materia Prima y Proveedores
 UPDATE permisos SET grupo = 'Materia Prima y Proveedores' WHERE id IN (36, 37, 38, 39);
+
+
+alter table productos
+    add grupo varchar(255) null;
+
+-- CHICHA
+UPDATE productos SET grupo = 'CHICHA' WHERE id IN (1, 4, 5, 6, 7, 8, 9, 18, 19, 22, 64, 65, 66, 67, 68);
+
+-- GARAPIÑA
+UPDATE productos SET grupo = 'GARAPIÑA' WHERE id IN (39, 54, 56, 57, 58, 59, 60);
