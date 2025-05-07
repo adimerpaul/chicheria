0c477002e6e61db175e4e63c657df6e8250c9251
adimerpaul <adimer101@gmail.com>
2025-05-07 04:22:49 -0400

    termianocuentas por cobrar exlce pdf y

--- chichafront/src/pages/Cxcdetalle.vue
+++ chichafront/src/pages/Cxcdetalle.vue
@@ -480,32 +480,6 @@ export default {
       window.open(url, '_blank');
     },
     exportExcel() {
-      // let data = [
-      //   {
-      //     sheet: "Adults",
-      //     columns: [
-      //       { label: "User", value: "user" }, // Top level data
-      //       { label: "Age", value: (row) => row.age + " years" }, // Custom format
-      //       { label: "Phone", value: (row) => (row.more ? row.more.phone || "" : "") }, // Run functions
-      //     ],
-      //     content: [
-      //       { user: "Andrea", age: 20, more: { phone: "11111111" } },
-      //       { user: "Luis", age: 21, more: { phone: "12345678" } },
-      //     ],
-      //   },
-      //   {
-      //     sheet: "Children",
-      //     columns: [
-      //       { label: "User", value: "user" }, // Top level data
-      //       { label: "Age", value: "age", format: '# "years"' }, // Column format
-      //       { label: "Phone", value: "more.phone", format: "(###) ###-####" }, // Deep props and column format
-      //     ],
-      //     content: [
-      //       { user: "Manuel", age: 16, more: { phone: 9999999900 } },
-      //       { user: "Ana", age: 17, more: { phone: 8765432135 } },
-      //     ],
-      //   },
-      // ]
       let data = [{
         sheet: "Cuentas por cobrar detalle",
         columns: [
